59b739ce61a3630b925cb70624c148b9409819d7
markus1bauer <markus1.bauer@tum.de>
2022-03-15 15:48:41 +0100

    Update format

--- R/_prepare_data.R
+++ R/_prepare_data.R
@@ -10,7 +10,13 @@ library(tidyverse)
 library(naniar)
 
 ### Start---------------------------------------------------------------------
-#installr::updateR(browse_news = F, install_R = T, copy_packages = T, copy_Rprofile.site = T, keep_old_packages = T, update_packages = T, start_new_R = F, quit_R = T)
+installr::updateR(browse_news = FALSE, install_R = TRUE, copy_packages = TRUE, copy_Rprofile.site = TRUE, keep_old_packages = TRUE, update_packages = TRUE, start_new_R = FALSE, quit_R = TRUE, print_R_versions = TRUE, GUI = TRUE)
+sessionInfo()
+remotes::install_github(file.path("inbo", "checklist"))
+checklist::setup_source()
+checklist::check_source()
+devtools::check()
+
 rm(list = ls())
 setwd(here("data", "raw"))
 
